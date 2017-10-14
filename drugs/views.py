69adb7b33b6f06f72771bad35d07e23b82cb378b
saloni1996 <salonibhartia1996@gmail.com>
2017-10-14 17:05:23 +0530

    Final Changes

--- drugs/views.py
+++ drugs/views.py
@@ -10,6 +10,12 @@ from rest_framework.decorators import api_view,permission_classes
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
+import bigchaindb_driver
+#bigchaindb_driver
+from bigchaindb_driver import BigchainDB
+from time import sleep
+from bigchaindb_driver.crypto import generate_keypair
+from sys import exit
 
 
 @api_view(['POST'])
@@ -31,7 +37,7 @@ def get_image(request):
 	im = im.convert('RGB')
 	im.save('imageToSave2.png')
 	text =' '
-	#res=''
+	res=''
 
 	if(name=='Digene0'):
 		#print "hello D"
@@ -69,6 +75,130 @@ def get_image(request):
 
 	print(text)
 	#print(res)
+
 	result = text + ',' + res
 	#print(result)
 	return HttpResponse(text)
+
+
+
+def block_chain(request):
+	alice, bob = generate_keypair(), generate_keypair()
+
+
+	bdb_root_url = 'http://localhost:9984'
+	bdb = BigchainDB(bdb_root_url)
+
+	drug = {
+	   'data': {
+	         'Digene': {
+	            ' aluminium hydroxide':'300 mg',
+	            'magnesium aluminium silicate':'50 mg',
+	             'magnesium hydroxide': '25 mg',
+	              'simethicone':'25 mg',
+	         },
+
+	         'Meftal Spas':{
+
+	         	'Norfloxacin IP':'250mg',
+	         	'Dicyclomine': '10mg',
+
+	         },
+	         'NORFLOX-TZ':
+	         {
+
+	         	'Norfloxacin IP':'400 mg',
+				'Tinidazole IP':'600 mg'
+
+	         },
+
+	         'Paracetamol':
+	         {
+	         	 'Paracetamol PhEur':'500mg',
+
+
+	         },
+	         'Zantac':
+	         {
+	         	'ranitidine HCl':'168 mg',
+
+	         },
+	     },
+	 }
+
+
+
+	prepared_creation_tx = bdb.transactions.prepare(
+	       operation='CREATE',
+	      signers=alice.public_key,
+	      asset=drug,
+	   )
+
+
+	fulfilled_creation_tx = bdb.transactions.fulfill(
+	     prepared_creation_tx, private_keys=alice.private_key)
+
+
+	'''
+	print(bdb.transactions.send(fulfilled_creation_tx))
+	if sent_creation_tx == fulfilled_creation_tx:
+	  print("HELLO")
+	'''
+
+	txid = fulfilled_creation_tx['id']
+
+	#print(txid)
+	#print(bdb.transactions.status(txid))
+
+
+	trials = 0
+	while trials < 60:
+	    try:
+	        if bdb.transactions.status(txid).get('status') == 'valid':
+	            print('Tx valid in:', trials, 'secs')
+	            break
+	    except bigchaindb_driver.exceptions.NotFoundError:
+	        trials += 1
+	        sleep(1)
+	
+	if trials == 60:
+	    print('Tx is still being processed... Bye!')
+	    exit(0)
+
+	asset_id = txid
+
+	transfer_asset = {
+	    'id': asset_id
+	}
+
+	output_index = 0
+	output = fulfilled_creation_tx['outputs'][output_index]
+
+	transfer_input = {
+	    'fulfillment': output['condition']['details'],
+	    'fulfills': {
+	        'output_index': output_index,
+	        'transaction_id': fulfilled_creation_tx['id']
+	    },
+	    'owners_before': output['public_keys']
+	}
+
+	prepared_transfer_tx = bdb.transactions.prepare(
+	    operation='TRANSFER',
+	    asset=transfer_asset,
+	    inputs=transfer_input,
+	    recipients=bob.public_key,
+	)
+
+	fulfilled_transfer_tx = bdb.transactions.fulfill(
+	    prepared_transfer_tx,
+	    private_keys=alice.private_key,
+	)
+
+	sent_transfer_tx = bdb.transactions.send(fulfilled_transfer_tx)
+
+	print("Is Bob the owner?",
+	    sent_transfer_tx['outputs'][0]['public_keys'][0] == bob.public_key)
+
+	print("Was Alice the previous owner?",
+	    fulfilled_transfer_tx['inputs'][0]['owners_before'][0] == alice.public_key)
